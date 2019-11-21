fb13b6b78033c7d5385a25f2c6a1edc4a564e64c
ben-han-cn <ben.han.cn@gmail.com>
2019-11-21 10:03:38 +0000

    expose main struct

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,9 @@ mod flag;
 pub mod node;
 pub mod node_chain;
 pub mod tree;
+
+pub use crate::{
+    node::NodePtr,
+    node_chain::NodeChain,
+    tree::{DomainTree, FindResult, FindResultFlag},
+};
